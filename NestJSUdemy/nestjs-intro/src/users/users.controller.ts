d0f2c23e24ada189ba683b59ffbe4caffbb87407
WatcharapolxFa <63010870@kmitl.ac.th>
2025-01-26 21:47:17 +0700

    feat:Comeback to learn

--- NestJSUdemy/nestjs-intro/src/users/users.controller.ts
+++ NestJSUdemy/nestjs-intro/src/users/users.controller.ts
@@ -1,22 +1,39 @@
-import { Controller,Get,Post,Param,Query, Body,Headers,Ip} from '@nestjs/common';
-import { request } from 'http';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  Headers,
+  Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    // * @Get() decorator is used to define a GET request handler.
-    @Get('/:id/:optional?')
-    public getUsers(@Param('id') id: any ,@Query('limit') limit: any){
-        console.log(id);
-        console.log(limit);
-        return 'You sent a Get request to users endpoint';
-    }
-    // * @Post() decorator is used to define a POST request handler.
-    @Post()
-    public createUsers(@Body('email') request: any,@Headers()headers: any ,@Ip() ip: any){
-        console.log(request);
-        console.log(headers);
-        console.log(ip);
-        return 'You sent a Post request to users endpoint';
-    }
-
+  // * @Get() decorator is used to define a GET request handler.
+  @Get('/:id')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit') limit: number ,
+    @Query('page') page: number,
+  ) {
+    console.log(typeof id);
+    console.log(id);
+    return 'You sent a Get request to users endpoint';
+  }
+  // * @Post() decorator is used to define a POST request handler.
+  @Post()
+  public createUsers(
+    @Body('email') request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(request);
+    console.log(headers);
+    console.log(ip);
+    return 'You sent a Post request to users endpoint';
+  }
 }
